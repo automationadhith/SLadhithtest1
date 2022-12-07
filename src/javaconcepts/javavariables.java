4729d6cc89353b7a080e80c6ac0734effebb7fb6
adhith94agmail <adhith94agmail@ip-172-31-29-65.ec2.internal>
2022-12-07 08:11:02 +0000

    "commit from eclipse"

---
+++ src/javaconcepts/javavariables.java
@@ -0,0 +1,13 @@
+package javaconcepts;
+
+public class javavariables {
+	int a=5;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		javavariables obj = new javavariables();
+				System.out.println(obj.a);
+
+	}
+
+}
